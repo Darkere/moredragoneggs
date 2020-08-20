51a4b78bf2f7f2e9a4f9b2a0ac4d4b8e26f7ea19
Darkere <Dark-277@web.de>
2020-08-20 23:45:52 +0200

    fix deathevent firing on both sides

--- src/main/java/com/darkere/moredragoneggs/MoreDragonEggs.java
+++ src/main/java/com/darkere/moredragoneggs/MoreDragonEggs.java
@@ -1,27 +1,15 @@
 package com.darkere.moredragoneggs;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.boss.dragon.EnderDragonEntity;
-import net.minecraft.world.dimension.EndDimension;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.World;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.gen.feature.EndPodiumFeature;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("moredragoneggs")
@@ -35,7 +23,11 @@ public class MoreDragonEggs {
 
     @SubscribeEvent
     public void LivingDeathEvent(LivingDeathEvent event) {
-        if(event.getEntity().getEntityWorld().getDimension() instanceof EndDimension){
+        if (event.getEntityLiving() == null) return;
+        MinecraftServer server = event.getEntity().getServer();
+        if (server == null) return;
+        if(server.getWorld(World.field_234918_g_) == null || server.getWorld(World.field_234918_g_).isRemote()) return;
+        if (event.getEntity().getEntityWorld().equals(server.getWorld(World.field_234920_i_))) {
             if (event.getEntity() instanceof EnderDragonEntity) {
                 EnderDragonEntity d = (EnderDragonEntity) event.getEntity();
                 if (d.getFightManager() != null && d.getFightManager().hasPreviouslyKilledDragon()) {
