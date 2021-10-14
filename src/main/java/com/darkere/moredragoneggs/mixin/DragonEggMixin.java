3e7ba7d8dadf2379c33ba80345f668488aae0a45
Darkere <Dark-277@web.de>
2021-10-14 12:43:33 +0200

    1.17.1

--- src/main/java/com/darkere/moredragoneggs/mixin/DragonEggMixin.java
+++ src/main/java/com/darkere/moredragoneggs/mixin/DragonEggMixin.java
@@ -1,11 +1,11 @@
 package com.darkere.moredragoneggs.mixin;
 
-import net.minecraft.block.Blocks;
-import net.minecraft.entity.boss.dragon.EnderDragonEntity;
-import net.minecraft.world.end.DragonFightManager;
-import net.minecraft.world.gen.Heightmap;
-import net.minecraft.world.gen.feature.EndPodiumFeature;
-import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
+import net.minecraft.world.level.dimension.end.EndDragonFight;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.levelgen.feature.EndPodiumFeature;
+import net.minecraft.server.level.ServerLevel;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(DragonFightManager.class)
+@Mixin(EndDragonFight.class)
 public class DragonEggMixin {
 
     @Shadow
@@ -22,13 +22,13 @@ public class DragonEggMixin {
     @Shadow
     @Final
     private
-    ServerWorld level;
+    ServerLevel level;
 
 
-    @Inject(at = @At("HEAD"),method = "setDragonKilled(Lnet/minecraft/entity/boss/dragon/EnderDragonEntity;)V")
-    private void setDragonKilled(EnderDragonEntity p_186096_1_, CallbackInfo cb) {
+    @Inject(at = @At("HEAD"),method = "setDragonKilled(Lnet/minecraft/world/entity/boss/enderdragon/EnderDragon;)V")
+    private void setDragonKilled(EnderDragon p_186096_1_, CallbackInfo cb) {
         if (this.previouslyKilled) {
-            this.level.setBlockAndUpdate(this.level.getHeightmapPos(Heightmap.Type.MOTION_BLOCKING, EndPodiumFeature.END_PODIUM_LOCATION), Blocks.DRAGON_EGG.defaultBlockState());
+            this.level.setBlockAndUpdate(this.level.getHeightmapPos(Heightmap.Types.MOTION_BLOCKING, EndPodiumFeature.END_PODIUM_LOCATION), Blocks.DRAGON_EGG.defaultBlockState());
         }
     }
 }
